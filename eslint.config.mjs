9957dece6152234f7730a3f97dc558778c29efc1
abhishekMaddhehshiya <abhishekmaddheshiya829@gmail.com>
2025-06-30 13:39:27 +0530

    fixed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    // This object defines custom rules or overrides for the entire config
+    rules: {
+      // Disable the 'no-explicit-any' rule
+      "@typescript-eslint/no-explicit-any": "off",
+      // You can also set it to 'warn' if you want it to be a warning instead of an error
+      // "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
 ];
 
 export default eslintConfig;
